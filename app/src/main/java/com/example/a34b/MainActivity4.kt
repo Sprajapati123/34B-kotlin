d0f1e2964793f097fe2bf6abceab2e72adab442c
sprajapati123 <sandisprajapati52@gmail.com>
2024-05-05 12:00:26 +0545

    intnet

--- app/src/main/java/com/example/a34b/MainActivity4.kt
+++ app/src/main/java/com/example/a34b/MainActivity4.kt
@@ -1,34 +1,24 @@
 package com.example.a34b
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class FirstActivity : AppCompatActivity() {
-    lateinit var button: Button
-    lateinit var editText: TextView
+class MainActivity4 : AppCompatActivity() {
+    lateinit var textView : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_first)
-        editText = findViewById(R.id.editTextText2)
+        setContentView(R.layout.activity_main4)
 
-        button = findViewById(R.id.button)
-        button.setOnClickListener {
-            var intent = Intent(this@FirstActivity,
-                MainActivity4::class.java)
+        textView = findViewById(R.id.textView3)
 
-            var email :String = editText.text.toString();
-            intent.putExtra("email",email)
-            startActivity(intent)
-//            finish()
-        }
+        var data : String? = intent.getStringExtra("email")
+
+        textView.text = "you have entered $data"
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -36,5 +26,4 @@ class FirstActivity : AppCompatActivity() {
             insets
         }
     }
-
 }
