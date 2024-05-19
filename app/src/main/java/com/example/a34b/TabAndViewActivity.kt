5663ff27ed1ba013db349ad7a90522db863e0298
sprajapati123 <sandisprajapati52@gmail.com>
2024-05-19 12:52:03 +0545

    tab and viewpager

--- app/src/main/java/com/example/a34b/TabAndViewActivity.kt
+++ app/src/main/java/com/example/a34b/TabAndViewActivity.kt
@@ -5,12 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentManager
+import com.example.a34b.adapter.TabAdapter
+import com.example.a34b.databinding.ActivityTabAndViewBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class TabAndViewActivity : AppCompatActivity() {
+    lateinit var tabAndViewBinding: ActivityTabAndViewBinding
+    var heading = arrayOf("Message","Status","Call")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tab_and_view)
+        tabAndViewBinding = ActivityTabAndViewBinding.inflate(layoutInflater)
+        setContentView(tabAndViewBinding.root)
+
+        var fragmentManager : FragmentManager = supportFragmentManager
+        tabAndViewBinding.viewPager2.adapter =
+            TabAdapter(fragmentManager,lifecycle)
+
+        TabLayoutMediator(tabAndViewBinding.tabLayout,
+            tabAndViewBinding.viewPager2){
+            tabs,position -> tabs.text = heading[position]
+        }.attach()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
